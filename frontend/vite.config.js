924e14dfdd9124fdf778f7d0e2cadfeceb13b7fe
jin <jin@gmail.com>
2025-06-02 18:31:58 +0530

    api update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://jaa2.onrender.com',
         changeOrigin: true,
       }
     }
